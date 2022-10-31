10888295a8855deccd9890c8910fbb6c515b870e
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2022-10-31 00:16:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement=document.getElementById('question-container')
+
+const startingSeconds = 60;
+const countElement = document.getElementById('countdown')
+
+function updateCountdown(){
+    time--;
+    const minutes =Math.floor(time / 60);
+}
+
+startButton.addEventListener('Click', startGame)
+
+function startGame(){
+    console.log('Started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
+
+
+function resetState(){
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
+        answerButtonElement.removeChild
+
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const question = [ 
+    {
+    question: 'What does HTML stand for?'
+    answer: [
+        {text: '', correct:true},
+        {text: '', correct:false},
+        {text: '', correct:false},
+        {text: '', correct:false},
+    ]
+    }
